4a7fde026f9098c2ae00eff09044b62e46629f32
Nimit Bhargava <nimitbhargava@gmail.com>
2017-09-17 01:47:04 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const puppeteer = require('puppeteer');
+
+(async() => {
+
+    const browser = await puppeteer.launch();
+const page = await browser.newPage();
+await page.goto('http://example.com');
+
+await browser.close();
+
+})();

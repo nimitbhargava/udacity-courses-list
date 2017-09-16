146f08ce02217ae2be5f3bff3225eb8bf6795b41
Nimit Bhargava <nimitbhargava@gmail.com>
2017-09-17 02:35:57 +0530

    Getting all courses

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ const page = await browser.newPage();
 await page.goto('http://www.udacity.com/courses/all');
 const DOMstring = await page.content();
 
+const cheerio = require('cheerio');
+const $ = cheerio.load(DOMstring);
+
+var courses = [];
+
+$('div[div[data-course-summaries] div[data-course-summary]').each(function () {
+    var id = $(this).attr('id');
+    console.log(id);
+})
+
 await browser.close();
 
 })();

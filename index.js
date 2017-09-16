1ea2fe6534fcf113bbac2fdc0a1dda224ce1c0a7
Nimit Bhargava <nimitbhargava@gmail.com>
2017-09-17 02:46:03 +0530

    Getting only new courses

--- index.js
+++ index.js
@@ -12,11 +12,14 @@ const DOMstring = await page.content();
 const cheerio = require('cheerio');
 const $ = cheerio.load(DOMstring);
 
-var courses = [];
 
-$('div[div[data-course-summaries] div[data-course-summary]').each(function () {
-    var id = $(this).attr('id');
-    console.log(id);
+$('div[data-course-summaries] div[data-course-summary]').each(function () {
+    var courseID = $(this).attr('id');
+    var coursePublished = $('#'+courseID+' .col-sm-9 .row .col-sm-8 .h-slim .badges .badge').data('cloak') === "" ? "new" : "old";
+    var courseName = $('#'+courseID+' .col-sm-9 .row .col-sm-8 .h-slim a').text().trim();
+    if(coursePublished === "new") {
+        console.log(courseID+' '+ courseName);
+    }
 })
 
 await browser.close();
